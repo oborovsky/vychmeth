ace7a9af3451185920d940a84c92f52a1bc7844f
ulihtenshtein <ulihtenshtein@gmail.com>
2015-09-20 00:13:10 +0600

    можно этот вариант попробовать сдать

--- task1/task1.cc
+++ task1/task1.cc
@@ -15,6 +15,15 @@ struct Interval
 {
 	double first;
 	double last;
+	// double cur{first};
+	// double operator++() 
+	// {
+	// 	return cur += m_step();
+	// };
+	// double operator*(){return cur;};
+	// double (*m_step)() = {[]()->double {return 0.01;}};
+	// //Interval() = default;
+	// Interval(double (*step)()):m_step{step} {};
 };
 
 ostream& operator<< (ostream &os, dvec& x)
@@ -45,7 +54,24 @@ dvec makeResultOnNode(dvec& x, const F& f)
 {
 	dvec y(x.size());
 	transform (x.begin(), x.end(), y.begin(), f);
-	return y;
+	return move(y);
+}
+double polynome(double a, dvec& x, dvec& y)
+{
+	double r = 0;
+	for (unsigned long i = 0; i < y.size();i++)
+	{
+		double w = 1;
+		for (unsigned long j = 0; j < x.size(); j++)
+		{
+			if( j != i) 
+			{
+				w *= a / (x[i] - x[j]) - x[j] / (x[i] - x[j]);
+			}	
+		}
+		r += y[i]*w;
+	}
+	return r;
 }
 void out(map<string, dvec>& opts)
 {
@@ -58,55 +84,41 @@ void out(map<string, dvec>& opts)
 			cout<<"невозможно создать файл"<<endl;
 			exit(1);
 		}
-	
+		dvec delts = opts["delts"];
+		auto delta = max_element(begin(delts), end(delts));
+		//cout<<"delta="<<*delta<<endl;
 		os<<"x="<<opts["x"]<<";"<<endl;
 		os<<"xh="<<opts["xh"]<<";"<<endl;
 	    os<<"y="<<opts["y"]<<";"<<endl;
 	    os<<"yy="<<opts["yy"]<<";"<<endl;
 	    os<<"yp="<<opts["yp"]<<";"<<endl;
-	    os<<"plot(x,y,xh,yp,xh,yy);"<<endl;
-	    //os<<"plot(x,yp);"<<endl;
+	    os<<"plot(xh,yy,'m--');"<<endl;
+	    os<<"plot(xh,yp,'r');"<<endl;
+	    os<<"plot(x,y,'*');"<<endl;
 	    os<<"xgrid();"<<endl;
+	    os<<"xtitle('delta="<<*delta<<"','X', 'Y');"<<endl;
 	}
 };
-double polynome(double a, dvec& x, dvec& y)
-{
-		double r = 0;
-		for (unsigned long i = 0; i < y.size();i++)
-		{
-			double w = 1;
-			for (unsigned long j = 0; j < x.size(); j++)
-			{
-				if( j != i) 
-				{
-					//if( a == x[j]) continue;
-					w *= a / (x[i] - x[j]) - x[j] / (x[i] - x[j]);
-				}	
-			}
-			r += y[i]*w;
-		}
-		return r;
-}
 
 int main(int argc, char const *argv[])
 {
 	
-	const double nodes = 10;
+	const double nodes = 5;
 	const double h = 0.01;
 	dvec x;
 	Interval interval = {-2,2};
 	auto func = [](double a)->double{return a*sin(a);};
 
-	makeNodeOfInterval(x, interval, [&]()->double{return (interval.last-interval.first)/nodes;});
+	makeNodeOfInterval(x, interval, [&]()->double{return (interval.last-interval.first)/(nodes-1);});
 	dvec&& y = makeResultOnNode(x, func);
 
 	using namespace std::placeholders;
-	auto ff = bind(polynome, _1, x, y);
+	auto lagrange = bind(polynome, _1, x, y);
 	auto step = [&h]()->double{return h;};
 
 	dvec xh;
 	makeNodeOfInterval(xh, interval,step);
-	dvec&& yp = makeResultOnNode(xh, ff);
+	dvec&& yp = makeResultOnNode(xh, lagrange);
 	dvec&& yy = makeResultOnNode(xh, func);
 
 	map<string, dvec> vvecs;
@@ -115,6 +127,18 @@ int main(int argc, char const *argv[])
 	vvecs["y"] = y;
 	vvecs["yy"] = yy;
 	vvecs["yp"] = yp;
+    dvec delts;
+    auto i = begin(yp);
+    auto j = begin(yy);
+    for(; i != end(yp) && j != end(yy) ; ++i, ++j) 
+    {
+    	double delta = abs(*i - *j);
+    	// cout << delta<<',';
+    	delts.push_back( delta );
+    }
+    // cout<<endl;
+    vvecs["delts"] = delts;
+
 
 	out(vvecs);
 
